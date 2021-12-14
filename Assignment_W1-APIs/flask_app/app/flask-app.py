9b8b65c4b71d8c7893bd2d84f3121b3eb651442f
Franky007Bond <frank.merwerth@gmx.de>
2021-12-14 22:28:36 +0100

    Assignemt Week 1 - Flask API

---
+++ Assignment_W1-APIs/flask_app/app/flask-app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, jsonify
+from bmi import bmi
+
+# instantiate Flask App
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def bmi_api():
+    '''
+    Function to calculate the BMI from the parameters that are handed over 
+    as json in the api call 
+    '''
+    params = request.get_json(force=True)
+
+    return jsonify(bmi(params["weight"], params["height"]))
+
+# driver
+if __name__ == '__main__':
+    app.run()
